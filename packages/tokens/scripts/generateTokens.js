c355d7a1fe5fece51b115e76122a5c60aaf947d4
최우석 <nick501@naver.com>
2026-04-02 18:09:08 +0900

    ♻️ Refactor: package.json에 type moudle 추가(css만 배포하기 때문)

--- packages/tokens/scripts/generateTokens.js
+++ packages/tokens/scripts/generateTokens.js
@@ -3,8 +3,8 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import postcss from 'postcss';
 import cssnano from 'cssnano';
+import postcss from 'postcss';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
