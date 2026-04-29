da3be1e9221f25a6ea75d66174e24cc7a1e21b0d
최우석 <nick501@naver.com>
2026-04-30 00:00:40 +0900

    🐛 Fix: toKebabCase 함수 변환 시 맨 앞 하이픈(-) 제거 로직 수정

--- scripts/stringUtils.mjs
+++ scripts/stringUtils.mjs
@@ -5,4 +5,4 @@ export const toCamelCase = (str) =>
   str.replace(/-([a-z])/g, (_, char) => char.toUpperCase());
 
 export const toKebabCase = (str) =>
-  str.replace(/([A-Z])/g, (char) => `-${char.toLowerCase()}`);
+  str.replace(/([A-Z])/g, (char) => `-${char.toLowerCase()}`).replace(/^-/, '');
