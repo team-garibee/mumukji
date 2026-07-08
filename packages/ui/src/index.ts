fb376fd2bfbd89eb00ecca129ab4a558510db862
aahreum <cocoding420@gmail.com>
2026-07-08 23:57:29 +0900

    ✨ Feat: each 유틸로 css 클래스 생성 및 배럴 파일 업데이트

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,1 +1,13 @@
-export {};
+import '@mumukji/tokens';
+import './styles/index.scss';
+
+export { SPACING_TOKENS } from './types';
+export { getSpacingClassName } from './utils';
+export type {
+  GapProps,
+  MarginProps,
+  PaddingProps,
+  Spacing,
+  SpacingProps,
+} from './types';
+export type { SpacingClassPrefix } from './utils';
