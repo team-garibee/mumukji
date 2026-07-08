e4603d6a29250593c06a4353f1d7503ed3fd0ff3
aahreum <cocoding420@gmail.com>
2026-07-08 23:37:18 +0900

    ⚙️ Chore: spacing 타입 추가

---
+++ packages/ui/src/types/index.ts
@@ -0,0 +1,9 @@
+export { SPACING_TOKENS } from './spacing';
+
+export type {
+  GapProps,
+  MarginProps,
+  PaddingProps,
+  Spacing,
+  SpacingProps,
+} from './spacing';
---
+++ packages/ui/src/types/spacing.ts
@@ -0,0 +1,49 @@
+export const SPACING_TOKENS = [
+  'null',
+  '2xs',
+  'xs',
+  'sm',
+  'md',
+  'lg',
+  'xl',
+  '2xl',
+  '3xl',
+  '4xl',
+  '5xl',
+  '6xl',
+  '7xl',
+  '8xl',
+  '9xl',
+  '10xl',
+  '11xl',
+] as const;
+
+export type Spacing = (typeof SPACING_TOKENS)[number];
+
+export interface PaddingProps {
+  p?: Spacing;
+  px?: Spacing;
+  py?: Spacing;
+  pt?: Spacing;
+  pr?: Spacing;
+  pb?: Spacing;
+  pl?: Spacing;
+}
+
+export interface MarginProps {
+  m?: Spacing;
+  mx?: Spacing;
+  my?: Spacing;
+  mt?: Spacing;
+  mr?: Spacing;
+  mb?: Spacing;
+  ml?: Spacing;
+}
+
+export interface GapProps {
+  gap?: Spacing;
+  columnGap?: Spacing;
+  rowGap?: Spacing;
+}
+
+export type SpacingProps = PaddingProps & MarginProps & GapProps;
